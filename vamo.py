ffc6372b4eeef50c21ed45bb6d72e63636b1b204
Bernardo Santinoni <133998636+Besanti@users.noreply.github.com>
2023-05-29 23:05:18 -0300

    Update vamo.py
    pfv vai

--- vamo.py
+++ vamo.py
@@ -1,8 +1,10 @@
 import csv
 import streamlit as st
 
-def listar_conteudo(starwars_csv):
-    with open(starwars_csv, 'r') as arquivo_csv:
+arquii = 'C: tidyverse/dplyr/data-raw/starwars.csv'
+
+def listar_conteudo(arquii):
+    with open(arquii, 'r') as arquivo_csv:
         leitor_csv = csv.reader(arquivo_csv)
         for linha in leitor_csv:
             st.write(linha)
