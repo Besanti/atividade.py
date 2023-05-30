e135f2998950352c62f547dde330b2c557fa8067
Bernardo Santinoni <133998636+Besanti@users.noreply.github.com>
2023-05-30 08:47:28 -0300

    Update teste_grafico.py
    botando o segundo grafico e fé em deus

--- teste_grafico.py
+++ teste_grafico.py
@@ -7,3 +7,9 @@ chart_data = pd.DataFrame(
     columns=["a", "b", "c"])
 
 st.bar_chart(chart_data)
+
+chart_datao = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_datao)
