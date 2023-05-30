10c0023b910db06e1692fcc1333e33f59ed855ca
Bernardo Santinoni <133998636+Besanti@users.noreply.github.com>
2023-05-30 09:02:41 -0300

    Update grafico_starwars.py
    sla

--- grafico_starwars.py
+++ grafico_starwars.py
@@ -5,10 +5,7 @@ import numpy as np
 df = pd.read_csv('starwars.csv')
 
 chart_data = pd.DataFrame(
-    np.starwars.cvs(20, 3),
+    np.starwars.cvs(2, 3),
     columns=["name", "height"])
 
-
-
 st.bar_chart(chart_data)
-
