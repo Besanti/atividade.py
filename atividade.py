f4a71c3a01e3befb70570fa20bfa953add36bb8d
Bernardo Santinoni <133998636+Besanti@users.noreply.github.com>
2023-05-30 08:02:27 -0300

    Update atividade.py
    ...

--- atividade.py
+++ atividade.py
@@ -1,4 +1,11 @@
 import csv
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
 df = pd.read_csv('starwars.csv')
+
+arq = open('starwars.csv', encoding='utf-8')
+for registro in csv.DictReader(arq, delimiter=','):
+
+  print(registro)
