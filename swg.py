3a7d3e48f13fce92d344f7b2bc4251970fa315d6
Bernardo Santinoni <133998636+Besanti@users.noreply.github.com>
2023-06-02 01:01:52 -0300

    Create swg.py

---
+++ swg.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from bokeh.plotting import figure
+
+import streamlit as st
+
+
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+p = figure(
+    title='simple line example',
+    x_axis_label='x',
+    y_axis_label='y')
+
+p.line(x, y, legend_label='Trend', line_width=2)
+
+st.bokeh_chart(p, use_container_width=True)
